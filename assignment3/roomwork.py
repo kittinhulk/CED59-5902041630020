19216217656e9b7c3e452e200bc9552074ccb72d
TheGod <50391612+kittinhulk@users.noreply.github.com>
2019-07-11 16:04:38 +0700

    Create roomwork.py

---
+++ assignment3/roomwork.py
@@ -0,0 +1,9 @@
+my_list = [25,25,50]
+print("sum list =",sum(my_list))
+
+my_tuble = (100)
+print("vir tuble =(",my_tuble,")")
+
+set1={5,77,9,12,80,90}
+set2={40,5,77,9,12,55}
+print("sets =",set1.intersection(set2))
